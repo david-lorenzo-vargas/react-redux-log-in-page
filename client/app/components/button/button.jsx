17be39790dee75ef8fac42d8406cf797b034af20
unknown <dlorenzo.05@gmail.com>
2020-11-25 19:49:49 +0100

    props received and button built

--- client/app/components/button/button.jsx
+++ client/app/components/button/button.jsx
@@ -5,8 +5,15 @@ import styles from './button.scss';
 const cx = classnames.bind(styles);
 
 const Button = (props) => {
+  const { theme, text } = props;
+
   return (
-  
+    <div className={cx('button', {
+      'button--blue': theme,
+    })}
+    >
+      <span>{text}</span>
+    </div>
   );
 };
 
