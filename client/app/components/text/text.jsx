8398daaa752649f997ddd84335366a8ef36dab8b
unknown <dlorenzo.05@gmail.com>
2020-11-25 19:51:22 +0100

    props received and text componentn built

--- client/app/components/text/text.jsx
+++ client/app/components/text/text.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import classnames from 'classnames/bind';
 import styles from './text.scss';
 
-const cx = classnames.bind(styles);
+// const cx = classnames.bind(styles);
 
 const Text = (props) => {
+  const { text } = props;
 
   return (
+    <div>
+      <span>{text}</span>
+    </div>
   );
 };
 
