e5c971b21f8e30740c7fee937fc4ad3b44d00b62
unknown <dlorenzo.05@gmail.com>
2020-11-25 18:54:02 +0100

    text.jsx file created for text component

---
+++ client/app/components/text/text.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import classnames from 'classnames/bind';
+import styles from './text.scss';
+
+const cx = classnames.bind(styles);
+
+const Text = (props) => {
+
+  return (
+  );
+};
+
+export default Text;
