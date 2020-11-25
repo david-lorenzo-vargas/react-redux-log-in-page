a01915eeea2b42cb5883b27184c8254f7d02ee13
unknown <dlorenzo.05@gmail.com>
2020-11-25 19:48:36 +0100

    components imported, structure built in return

--- client/app/App.jsx
+++ client/app/App.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import { Row, Column } from './components/grid';
+import Input from './components/input';
+import Text from './components/text';
+import Button from './components/button';
 import styles from './reset.scss';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+const App = () => (
+  <div className={styles.app}>
+    <Row>
+      <Column>
+        <Text text="Log in" />
+      </Column>
+      <div className={styles.login__input}>
+        <Column>
+          <Input placeHolder="user name" />
+          <Input placeHolder="email" />
+          <Input placeHolder="password" />
+          <Button theme="blue" text="Log In" />
+        </Column>
+      </div>
 
-    };
+    </Row>
+  </div>
 
-  render() {
-    return ();
-  }
-}
+);
 
 export default App;
