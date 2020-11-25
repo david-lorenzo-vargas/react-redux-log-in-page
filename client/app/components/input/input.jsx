4a1f725d53d6d62471e4fdc4549c0e744bec7bce
unknown <dlorenzo.05@gmail.com>
2020-11-25 19:11:44 +0100

    input.jsx file created for input component

---
+++ client/app/components/input/input.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './input.scss';
+
+const Input = (props) => {
+  return (
+    <div className={styles.input}>
+      <input type="text" />
+    </div>
+  );
+};
+
+export default Input;
