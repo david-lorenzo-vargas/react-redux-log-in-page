e8ded968b977294bc77b4313d55683a48333be02
unknown <dlorenzo.05@gmail.com>
2020-11-25 18:47:53 +0100

    App.jsx file created for rendering the app

---
+++ client/app/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Row, Column } from './components/grid';
+import styles from './reset.scss';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    };
+
+  render() {
+    return ();
+  }
+}
+
+export default App;
