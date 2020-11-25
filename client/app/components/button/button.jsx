281df839bbecedd244d9ab375b1a3389904f31e7
unknown <dlorenzo.05@gmail.com>
2020-11-25 18:49:22 +0100

    button.jsx file created for Button component

---
+++ client/app/components/button/button.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import classnames from 'classnames/bind';
+import styles from './button.scss';
+
+const cx = classnames.bind(styles);
+
+const Button = (props) => {
+  return (
+  
+  );
+};
+
+export default Button;
