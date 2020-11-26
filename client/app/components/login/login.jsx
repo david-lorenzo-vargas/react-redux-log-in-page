16bfe43d14f8fe944b72d0cc6bde3014db5fd479
unknown <dlorenzo.05@gmail.com>
2020-11-26 14:00:17 +0100

    component converted into a class, redux imported, data fetched from api, event added to button component and fetchUsers function called, maoStateProps function created for getinh the stoe from redux,maoDispatchToProps function created for gettinh actions from redux, both functions connected when export default

--- client/app/components/login/login.jsx
+++ client/app/components/login/login.jsx
@@ -1,14 +1,53 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from '@reduxjs/toolkit';
+import actions from './state/users.actions';
 import Input from '../input';
 import Button from '../button';
+import usersApi from './state/api';
 
-const Login = () => (
-  <>
-    <Input placeHolder="user name" type="text" />
-    <Input placeHolder="email" type="text" />
-    <Input placeHolder="password" type="password" />
-    <Button theme="blue" text="Log In" />
-  </>
-);
+class Login extends React.Components() {
+  constructor(props) {
+    super(props);
 
-export default Login;
+    this.handleButtonClick = this.handleButtonClick.bind(this);
+  }
+
+  hanldeButtonClick() {
+    this.fetchUsers();
+  }
+
+  fetchUsers() {
+    usersApi.get()
+      .then(response => response.json())
+      .then(data => this.fetchUsersSuccess(data))
+      .catch(() => this.fetchUsersRejected());
+  }
+
+  fetchUsersSuccess(data) {
+    this.props.actions.fetchUsersSuccess(data);
+  }
+
+  render() {
+    return (
+      <>
+        <Input placeHolder="user name" type="text" />
+        <Input placeHolder="email" type="text" />
+        <Input placeHolder="password" type="password" />
+        <Button onClick={this.handleButtonClick} theme="blue" text="Log In" />
+      </>
+    );
+  }
+}
+
+const mapStateToProps = (store) => ({
+  state: {
+    ...store.sendButton,
+  },
+});
+
+const mapDispatchToProps = dispatch => ({
+  actions: bindActionCreators(actions, dispatch),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
