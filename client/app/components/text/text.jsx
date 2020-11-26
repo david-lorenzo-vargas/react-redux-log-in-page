3151aca376860239c5c35641cbc5804d5e5d79ed
unknown <dlorenzo.05@gmail.com>
2020-11-26 11:31:54 +0100

    props received and styles given dynamicly

--- client/app/components/text/text.jsx
+++ client/app/components/text/text.jsx
@@ -2,13 +2,27 @@ import React from 'react';
 import classnames from 'classnames/bind';
 import styles from './text.scss';
 
-// const cx = classnames.bind(styles);
+const cx = classnames.bind(styles);
 
 const Text = (props) => {
-  const { text } = props;
+  const {
+    text,
+    bold,
+    theme,
+    size,
+    type,
+    cursive,
+  } = props;
 
   return (
-    <div>
+    <div className={cx('text', {
+      'text--bold': bold,
+      [`text--theme-${theme}`]: theme,
+      [`text--size-${size}`]: size,
+      [`text--type-${type}`]: type,
+      'text--cursive': cursive,
+    })}
+    >
       <span>{text}</span>
     </div>
   );
