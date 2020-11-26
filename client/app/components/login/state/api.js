7837488f822b756d4b06292ebfc1ae3b68105597
unknown <dlorenzo.05@gmail.com>
2020-11-26 21:47:28 +0100

    try catch logic added to face bugs in Promises

--- client/app/components/login/state/api.js
+++ client/app/components/login/state/api.js
@@ -1,13 +1,17 @@
 import users from './users.api.mock';
 
-const apiCall = (fn) => () => new Promise((resolve) => {
-  window.setTimeout(() => {
-    const response = {
-      json: () => fn(),
-    };
+const apiCall = (fn) => () => new Promise((resolve, reject) => {
+  try {
+    window.setTimeout(() => {
+      const response = {
+        json: () => fn(),
+      };
 
-    resolve(response);
-  }, 2000);
+      resolve(response);
+    }, 2000);
+  } catch(e) {
+    reject(e);
+  }
 });
 
 const get = apiCall(() => ({
@@ -15,4 +19,6 @@ const get = apiCall(() => ({
   usersArray: users,
 }));
 
-export default get;
+export default {
+  get
+};
