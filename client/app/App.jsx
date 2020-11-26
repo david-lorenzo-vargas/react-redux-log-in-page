00fb1982b468fc72dbbc3176dde7acae3198e88b
unknown <dlorenzo.05@gmail.com>
2020-11-26 11:39:17 +0100

    lint fixed

--- client/app/App.jsx
+++ client/app/App.jsx
@@ -12,9 +12,23 @@ const App = () => (
       <div className={styles.app__info}>
         <Column>
           <Logo size="medium" />
-          <Text text="blue vera" bold="bold" theme="blue" size="extra-big" type="uppercase" />
-          <Text text="Log in" theme="blue" size="big" />
-          <Text text="Lorem ipsum dolor sit amet, consectetur adipiscing elit." size="extra-small" cursive="italic" />
+          <Text
+            text="blue vera"
+            bold="bold"
+            theme="blue"
+            size="extra-big"
+            type="uppercase"
+          />
+          <Text
+            text="Log in"
+            theme="blue"
+            size="big"
+          />
+          <Text
+            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            size="extra-small"
+            cursive="italic"
+          />
         </Column>
       </div>
       <div className={styles.app__input}>
