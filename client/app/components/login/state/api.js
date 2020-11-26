201b29f25df5c0b23c6c0bbdd8f3853d6ffed70c
unknown <dlorenzo.05@gmail.com>
2020-11-26 13:00:40 +0100

    api.js file creatred for a fake call to an api by a promise, users array imported and get it once the response is succesfull, we pass the array of users

---
+++ client/app/components/login/state/api.js
@@ -0,0 +1,18 @@
+import users from './users.api.mock';
+
+const apiCall = (fn) => () => new Promise((resolve) => {
+  window.setTimeout(() => {
+    const response = {
+      json: () => fn(),
+    };
+
+    resolve(response);
+  }, 2000);
+});
+
+const get = apiCall(() => ({
+  success: true,
+  users,
+}));
+
+export default get;
