99341c2a886190eac4614620f75c12a8f763716a
unknown <dlorenzo.05@gmail.com>
2020-11-26 16:10:49 +0100

    onClick prop received

--- client/app/components/button/button.jsx
+++ client/app/components/button/button.jsx
@@ -5,12 +5,18 @@ import styles from './button.scss';
 const cx = classnames.bind(styles);
 
 const Button = (props) => {
-  const { theme, text } = props;
+  const { theme, text, onClick } = props;
+
+  const handleButtonClick = () => {
+    onClick();
+  };
 
   return (
-    <div className={cx('button', {
-      'button--blue': theme,
-    })}
+    <div
+      onClick={handleButtonClick}
+      className={cx('button', {
+        'button--blue': theme,
+      })}
     >
       <span>{text}</span>
     </div>
