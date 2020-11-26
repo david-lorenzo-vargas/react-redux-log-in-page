caf53d9aa2d66002f4de470da1aded17a6ebdc1d
unknown <dlorenzo.05@gmail.com>
2020-11-26 12:59:05 +0100

    users.api.mock.js file created with an array of objects, each object mocks a user that is saved in the data base

---
+++ client/app/components/login/state/users.api.mock.js
@@ -0,0 +1,29 @@
+const users = [
+  {
+    username: 'John Doe',
+    email: 'john-doe@mail.com',
+    password: '123JohnDoe',
+  },
+  {
+    username: 'Arthur Smith',
+    email: 'arthur.smith-email@email.com',
+    password: 'thisisnotasecurepassword',
+  },
+  {
+    username: 'Mike Mikerson',
+    email: 'mickey_m@myemail.com',
+    password: 'mickeyMouse1972',
+  },
+  {
+    username: 'Donald Duke',
+    email: 'd.duke@mail.com',
+    password: '123456789D',
+  },
+  {
+    username: 'Tom Thanks',
+    email: 'tom_thanks_you@email.com',
+    password: 'Tom1Thanks2!',
+  },
+];
+
+export default users;
