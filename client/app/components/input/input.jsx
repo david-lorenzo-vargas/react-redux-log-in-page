cec7f0c659c23cdd3524b7cc8a65cad0a2a69247
unknown <dlorenzo.05@gmail.com>
2020-11-26 18:41:03 +0100

    onChnage prop received and called in handleOnchange function

--- client/app/components/input/input.jsx
+++ client/app/components/input/input.jsx
@@ -2,11 +2,20 @@ import React from 'react';
 import styles from './input.scss';
 
 const Input = (props) => {
-  const { placeHolder, type } = props;
+  const { placeHolder, type, onChange } = props;
+
+  const handleOnChange = () => {
+    onChange();
+  };
 
   return (
     <div className={styles.input}>
-      <input className={styles.input__box} type={type} placeholder={placeHolder} />
+      <input
+        onChange={handleOnChange}
+        className={styles.input__box}
+        type={type}
+        placeholder={placeHolder}
+      />
     </div>
   );
 };
