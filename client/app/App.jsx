83206624942fda231628eeb21d06de94c35416b5
unknown <dlorenzo.05@gmail.com>
2020-11-26 12:44:43 +0100

    input,logo,text and button components removed, login and banner components imported an rendered for better organization of the App component

--- client/app/App.jsx
+++ client/app/App.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import { Row, Column } from './components/grid';
-import Input from './components/input';
-import Text from './components/text';
-import Button from './components/button';
-import Logo from './components/logo';
+import Login from './components/login';
+import Banner from './components/banner';
+
 import styles from './reset.scss';
 
 const App = () => (
@@ -11,32 +10,12 @@ const App = () => (
     <Row center>
       <div className={styles.app__info}>
         <Column>
-          <Logo size="medium" />
-          <Text
-            text="blue vera"
-            bold="bold"
-            theme="blue"
-            size="extra-big"
-            type="uppercase"
-          />
-          <Text
-            text="Log in"
-            theme="blue"
-            size="big"
-          />
-          <Text
-            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-            size="extra-small"
-            cursive="italic"
-          />
+          <Banner />
         </Column>
       </div>
       <div className={styles.app__input}>
         <Column>
-          <Input placeHolder="user name" type="text" />
-          <Input placeHolder="email" type="text" />
-          <Input placeHolder="password" type="password" />
-          <Button theme="blue" text="Log In" />
+          <Login />
         </Column>
       </div>
     </Row>
