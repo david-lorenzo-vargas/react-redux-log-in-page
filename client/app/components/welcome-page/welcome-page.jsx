4ae060f93ec8a28edaabfb9a32c0564b97333248
unknown <dlorenzo.05@gmail.com>
2020-11-27 21:56:32 +0100

    props received from Redux state, value recived from props passed as dynamic variale inside text

--- client/app/components/welcome-page/welcome-page.jsx
+++ client/app/components/welcome-page/welcome-page.jsx
@@ -1,22 +1,35 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Row, Column } from '../grid';
 import Text from '../text';
 
-const WelcomePage = () => (
-  <>
-    <Row>
-      <Column>
-        <Text
-          text="Welcome you are logged in"
-          theme="white"
-          cursive="cursive"
-          type="uppercase"
-          size="extra-big"
-          bold="bold"
-        />
-      </Column>
-    </Row>
-  </>
-);
+const WelcomePage = (props) => {
+  const { state } = props;
+  const { userNameLogged } = state;
 
-export default WelcomePage;
+  return (
+    <>
+      <Row>
+        <Column>
+          <Text
+            text={`Welcome ${userNameLogged}! you are logged in`}
+            theme="white"
+            cursive="cursive"
+            type="uppercase"
+            size="extra-big"
+            bold="bold"
+          />
+        </Column>
+      </Row>
+    </>
+  );
+};
+
+const mapStateToProps = (store) => ({
+  state: {
+    ...store.sendButton.users,
+    ...store.input,
+  },
+});
+
+export default connect(mapStateToProps)(WelcomePage);
