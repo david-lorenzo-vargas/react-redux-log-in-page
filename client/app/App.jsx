690b22e17e5759e07d3069639249772486f0eb84
unknown <dlorenzo.05@gmail.com>
2020-11-27 19:24:37 +0100

    connect imported from redux, bindcreatos imported from redux toolkit, actions imported from actions, WelcomePage component imported, props received from Redux, ternary operator in return to render the right component if the user is logged or not, mapStateToProps function created to use props coming from Redux, mapDispatchToProps function created to receive actions from Redux, both functions connected in export default

--- client/app/App.jsx
+++ client/app/App.jsx
@@ -1,26 +1,55 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from '@reduxjs/toolkit';
+import actions from './components/login/state/users.actions';
 import { Row, Column } from './components/grid';
 import Login from './components/login';
 import Banner from './components/banner';
+import WelcomePage from './components/welcome-page';
 
 import styles from './reset.scss';
 
-const App = () => (
-  <div className={styles.app}>
-    <Row center>
-      <div className={styles.app__info}>
-        <Column>
-          <Banner />
-        </Column>
-      </div>
-      <div className={styles.app__input}>
-        <Column>
-          <Login />
-        </Column>
-      </div>
-    </Row>
-  </div>
+const App = (props) => {
+  const { state } = props;
+  const { userLoggedIn } = state;
 
-);
+  return (
+    <div className={styles.app}>
+      <Row center>
+        <div className={styles.app__info}>
+          <Column>
+            <Banner />
+          </Column>
+        </div>
+        {!userLoggedIn ?
+          (
+            <div className={styles.app__input}>
+              <Column>
+                <Login />
+              </Column>
+            </div>
+          ) :
+          (
+            <div className={styles.app__welcome}>
+              <Column>
+                <WelcomePage />
+              </Column>
+            </div>
+          )}
+      </Row>
+    </div>
+  );
+};
 
-export default App;
+const mapStateToProps = (store) => ({
+  state: {
+    ...store.sendButton.users,
+    ...store.input,
+  },
+});
+
+const mapDispatchToProps = dispatch => ({
+  actions: bindActionCreators(actions, dispatch),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
