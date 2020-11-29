8666646f9076b1e2df96f3abde5a884e8ec2170e
unknown <dlorenzo.05@gmail.com>
2020-11-29 20:09:43 +0100

    RegisterForm component imported, logic added in render to render the right component depending on props coming from Redux state

--- client/app/App.jsx
+++ client/app/App.jsx
@@ -3,39 +3,43 @@ import { connect } from 'react-redux';
 import { Row, Column } from './components/grid';
 import Login from './components/login';
 import Banner from './components/banner';
+import RegisterForm from './components/register-form';
 import WelcomePage from './components/welcome-page';
 
 import styles from './reset.scss';
 
 const App = (props) => {
   const { state } = props;
-  const { userLoggedIn } = state;
+  const { userLoggedIn, registerStarted } = state;
 
   return (
-    <div className={styles.app}>
-      <Row center>
-        <div className={styles.app__info}>
-          <Column>
-            <Banner />
-          </Column>
+    registerStarted ? <RegisterForm /> :
+      (
+        <div className={styles.app}>
+          <Row center>
+            <div className={styles.app__info}>
+              <Column>
+                <Banner />
+              </Column>
+            </div>
+            {!userLoggedIn ?
+              (
+                <div className={styles.app__input}>
+                  <Column>
+                    <Login />
+                  </Column>
+                </div>
+              ) :
+              (
+                <div className={styles.app__welcome}>
+                  <Column>
+                    <WelcomePage />
+                  </Column>
+                </div>
+              )}
+          </Row>
         </div>
-        {!userLoggedIn ?
-          (
-            <div className={styles.app__input}>
-              <Column>
-                <Login />
-              </Column>
-            </div>
-          ) :
-          (
-            <div className={styles.app__welcome}>
-              <Column>
-                <WelcomePage />
-              </Column>
-            </div>
-          )}
-      </Row>
-    </div>
+      )
   );
 };
 
