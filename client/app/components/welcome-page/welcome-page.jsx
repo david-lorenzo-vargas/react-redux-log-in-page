59a7e012171b45099a53e8220e3c6c554b2dc5b6
unknown <dlorenzo.05@gmail.com>
2020-11-30 13:33:57 +0100

    logic added to render the component only when necessary

--- client/app/components/welcome-page/welcome-page.jsx
+++ client/app/components/welcome-page/welcome-page.jsx
@@ -1,21 +1,37 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import Banner from '../banner';
 import Login from '../login';
 import { Row, Column } from '../grid';
 import styles from './welcome-page.scss';
 
-const WelcomePage = () => (
-  <div className={styles.welcome}>
-    <Row>
-      <Column>
-        <Banner />
-      </Column>
-      <Column>
-        <Login />
-      </Column>
-    </Row>
-  </div>
+const WelcomePage = (props) => {
+  const { state } = props;
+  const { registerStatus } = state;
 
-);
+  return (
+    registerStatus !== 'started' ?
+      (
+        <div className={styles.welcome}>
+          <Row>
+            <Column>
+              <Banner />
+            </Column>
+            <Column>
+              <Login />
+            </Column>
+          </Row>
+        </div>
+      ) :
+      ''
+  );
+};
 
-export default WelcomePage;
+const mapStateToProps = (store) => ({
+  state: {
+    ...store.sendButton.users,
+    ...store.input,
+  },
+});
+
+export default connect(mapStateToProps)(WelcomePage);
