f58eb06bcee296ca5a861bfceae5be3660fd6041
unknown <dlorenzo.05@gmail.com>
2020-11-30 13:08:20 +0100

    spinner.jsx file created for spinner component

---
+++ client/app/components/spinner/spinner.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './spinner.scss';
+
+const Spinner = () => (
+  <div className={styles.spinner}>
+    <div className={styles.spinner__item} />
+  </div>
+);
+
+export default Spinner;
