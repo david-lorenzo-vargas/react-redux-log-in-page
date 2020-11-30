a1046573b837e45bd61844d75c4704fcaf542634
unknown <dlorenzo.05@gmail.com>
2020-11-30 13:09:52 +0100

    components imported, styles imported and render restructured

--- client/app/components/welcome-page/welcome-page.jsx
+++ client/app/components/welcome-page/welcome-page.jsx
@@ -1,35 +1,21 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import Banner from '../banner';
+import Login from '../login';
 import { Row, Column } from '../grid';
-import Text from '../text';
+import styles from './welcome-page.scss';
 
-const WelcomePage = (props) => {
-  const { state } = props;
-  const { userNameLogged } = state;
+const WelcomePage = () => (
+  <div className={styles.welcome}>
+    <Row>
+      <Column>
+        <Banner />
+      </Column>
+      <Column>
+        <Login />
+      </Column>
+    </Row>
+  </div>
 
-  return (
-    <>
-      <Row>
-        <Column>
-          <Text
-            text={`Welcome ${userNameLogged}! you are logged in`}
-            theme="white"
-            cursive="cursive"
-            type="uppercase"
-            size="extra-big"
-            bold="bold"
-          />
-        </Column>
-      </Row>
-    </>
-  );
-};
+);
 
-const mapStateToProps = (store) => ({
-  state: {
-    ...store.sendButton.users,
-    ...store.input,
-  },
-});
-
-export default connect(mapStateToProps)(WelcomePage);
+export default WelcomePage;
