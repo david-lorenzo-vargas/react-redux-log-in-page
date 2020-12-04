101daaffed48fe87d6843a5e9775e58f264a6acd
unknown <dlorenzo.05@gmail.com>
2020-12-04 19:39:21 +0100

    forgotten-password.jsx file created for forgotten-password component, input, button, row and column components improted, connect, action improted from Redux.

---
+++ client/app/components/forgotten-password/forgotten-password.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from '@reduxjs/toolkit';
+import actions from '../login/state/users.actions';
+import styles from './forgotten-password.scss';
+import Input from '../input';
+import Button from '../button';
+import { Row, Column } from '../grid';
+
+const ForgottenPassword = (props) => {
+  const { forgottenEmail } = props;
+
+  const handleForgottenPasswordlInput = (value) => {
+    props.actions.handleForgottenPasswordlInput(value);
+  };
+
+  const handleSubmitForgottenPasswordButtonClick = () => {
+    props.actions.handleSubmitForgottenPasswordButtonClick();
+  };
+
+  return (
+    <div className={styles.forgotten}>
+      <Row>
+        <Column>
+          <Input
+            onChange={handleForgottenPasswordlInput}
+            placeHolder="email"
+            type="text"
+            value={forgottenEmail}
+          />
+        </Column>
+        <Column>
+          <Button
+            onClick={handleSubmitForgottenPasswordButtonClick}
+            theme="blue"
+            text="Submit"
+          />
+        </Column>
+      </Row>
+    </div>
+  );
+};
+
+const mapStateToProps = (store) => ({
+  state: {
+    ...store.sendButton.users,
+    ...store.input,
+  },
+});
+
+const mapDispatchToProps = dispatch => ({
+  actions: bindActionCreators(actions, dispatch),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ForgottenPassword);
